65e97358f248eac6d5169c1f092773f8b4b379c3
Eden <cheungedenhk@gmail.com>
2025-02-08 13:32:50 +0000

    player movement script

--- scripts/player-controller.gd
+++ scripts/player-controller.gd
@@ -1,45 +1,219 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const ACCELERATION = 3000
+const MAX_SPEED = 10000
+const MAX_FALL_SPEED = 150
+const JUMP_HEIGHT = 25000
+const MIN_JUMP_HEIGHT = 12000
+const MAX_COYOTE_TIME = 6
+const MAX_COYOTE_WALL_TIME = 6
+const JUMP_BUFFER_TIME = 10
+const WALL_JUMP_AMOUNT = 12000
+const WALL_JUMP_TIME = 10
+const WALL_SLIDE_FACTOR = 0.8
+const WALL_HORIZONTAL_TIME = 30
+const GRAVITY = 1500
+const WEAK_GRAVITY = 500
+const DASH_SPEED = 10000
+const CLIMB_SPEED = 10000
+
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var ray_cast = $RayCast2D
+#@onready var ray_cast = $RayCast2D
+
+var input_axis = Vector2()
+
+var coyoteTimer = 0
+var coyoteWallTimer = 0
+var jumpBufferTimer = 0
+var wallJumpTimer = 0
+var wallHorizontalTimer = 0
+var dashTime = 0
+
+var spriteColor = "red"
+var canJump = false
+var friction = false
+var wall_sliding = false
+var trail = false
+var isDashing = false
+var hasDashed = false
+var isGrabbing = false
 
 func _physics_process(delta):
-	# Apply gravity if not on floor.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Horizontal movement.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
-		animated_sprite_2d.flip_h = direction < 0
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	
-	# Play proper animation.
-	if is_on_floor():
-		if direction:
-			animated_sprite_2d.play("run")
+	if !isDashing:
+		if velocity.y < MAX_FALL_SPEED:
+			velocity.y += GRAVITY * delta
 		else:
-			animated_sprite_2d.play("idle")
+			velocity.y += WEAK_GRAVITY * delta
+	
+	friction = false
+	
+	getInputAxis()
+	#dash(delta)
+	
+	wallSlide(delta)
+
+	if wallJumpTimer > WALL_JUMP_TIME:
+		wallJumpTimer = WALL_JUMP_AMOUNT
+		if !isDashing && !isGrabbing:
+			horizontalMovement(delta)
 	else:
-		animated_sprite_2d.play("jump")
+		wallJumpTimer += 1
 	
-	# Use move_and_slide (which updates is_on_floor() automatically)
+	if !canJump:
+		if !wall_sliding:
+			if velocity.y >= 0:
+				$AnimatedSprite2D.play("fall")
+			elif velocity.y < 0:
+				$AnimatedSprite2D.play("jump")
+				
+	if is_on_floor():
+		canJump = true
+		coyoteTimer = 0
+	else:
+		coyoteTimer += 1
+		if coyoteTimer > MAX_COYOTE_TIME:
+			canJump = false
+			coyoteTimer = 0
+		friction = true
+	
+	jumpBuffer(delta)
+
+	if Input.is_action_just_pressed("jump"):
+		if canJump:
+			jump(delta)
+			frictionOnAir()
+		else:
+			if $WallCast.is_colliding():
+				wallJump(delta)
+			frictionOnAir()
+			jumpBufferTimer = JUMP_BUFFER_TIME #amount of frame
+
+	setJumpHeight(delta)
+	jumpBuffer(delta)
+		
 	move_and_slide()
+	if sign(velocity.x)<0: $AnimatedSprite2D.flip_h = true
+	elif sign(velocity.x)>0: $AnimatedSprite2D.flip_h = false
+
+
+func jump(delta):
+	velocity.y = -JUMP_HEIGHT * delta
+
+func wallJump(delta):
+	wallJumpTimer = 0
+	velocity.x = -WALL_JUMP_AMOUNT * $WallCast.scale.x * delta
+	velocity.y = -JUMP_HEIGHT * delta
+	$WallCast.scale.x = -$WallCast.scale.x
+
+func wallSlide(delta):
+	if !canJump:
+		if $WallCast.is_colliding():
+			coyoteWallTimer = 0
+			wall_sliding = true
+			if Input.is_action_pressed("grab"):
+				isGrabbing = true
+				if input_axis.y != 0:
+					velocity.y = input_axis.y * CLIMB_SPEED * delta
+					$AnimatedSprite2D.play("climbing")
+				else:
+					velocity.y = 0
+					$AnimatedSprite2D.play("sliding")
+			else:
+				isGrabbing = false
+				velocity.y = velocity.y * WALL_SLIDE_FACTOR
+				$AnimatedSprite2D.play("sliding")
+		else:
+			wall_sliding = false
+			isGrabbing = false
+			coyoteWallTimer += 1
+			if coyoteWallTimer < MAX_COYOTE_WALL_TIME:
+				if input_axis.y != 0:
+					velocity.y = input_axis.y * CLIMB_SPEED * delta
+					$AnimatedSprite2D.play("climbing")
 	
-	if ray_cast.is_colliding():
-		if ray_cast.get_collider().is_in_group("Danger"):
-			Engine.time_scale = 0.5
-			$CollisionShape2D.queue_free() if $CollisionShape2D else {}
-			$Timer.start()
+
+func frictionOnAir():
+	if friction:
+		velocity.x = lerp(velocity.x, 0.0, 0.01)
+
+func jumpBuffer(delta):
+	if jumpBufferTimer > 0:
+		if is_on_floor():
+			jump(delta)
+		jumpBufferTimer -= 1
+
+func setJumpHeight(delta):
+	if Input.is_action_just_released("move_up"):
+		if velocity.y < -MIN_JUMP_HEIGHT * delta:
+			velocity.y = -MIN_JUMP_HEIGHT * delta
+
+func horizontalMovement(delta):
+	if Input.is_action_pressed("move_right"):
+		if $WallCast.is_colliding():
+			#await (get_tree().create_timer(0.1),"timeout")
+			velocity.x = min(velocity.x + ACCELERATION * delta, MAX_SPEED * delta)
+			$WallCast.scale.x = 1
+			if canJump:
+				$AnimatedSprite2D.play("run")
+		else:
+			velocity.x = min(velocity.x + ACCELERATION * delta, MAX_SPEED * delta)
+			$WallCast.scale.x = 1
+			if canJump:
+				$AnimatedSprite2D.play("run")
+
+	elif Input.is_action_pressed("move_left"):
+		if $WallCast.is_colliding():
+			#await (get_tree().create_timer(0.1),"timeout")
+			velocity.x = max(velocity.x - ACCELERATION * delta, -MAX_SPEED * delta)
+			$WallCast.scale.x = -1
+			if canJump:
+				$AnimatedSprite2D.play("run")
+		else:
+			velocity.x = max(velocity.x - ACCELERATION * delta, -MAX_SPEED * delta)
+			$WallCast.scale.x = -1
+			if canJump:
+				$AnimatedSprite2D.play("run")
+	else:
+		velocity.x = lerp(velocity.x, 0.0, 0.4)
+		if canJump:
+			$AnimatedSprite2D.play("idle")
+
+
+func dash(delta):
+	if !hasDashed:
+		if Input.is_action_just_pressed("dash"):
+			velocity = input_axis * DASH_SPEED * delta
+			spriteColor = "blue"
+			Input.start_joy_vibration(0, 1, 1, 0.2)
+			isDashing = true
+			hasDashed = true
+
+	if isDashing:
+		trail = true
+		dashTime += 1
+		if dashTime >= int(0.25 * 1 / delta):
+			isDashing = false
+			trail = false
+			dashTime = 0
+
+	if is_on_floor() && velocity.y >= 0:
+		hasDashed = false
+		spriteColor = "red"
+
+func getInputAxis():
+	input_axis = Vector2.ZERO
+	input_axis.x = int(Input.is_action_pressed("move_left")) - int(Input.is_action_pressed("move_right"))
+	input_axis.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
+	input_axis = input_axis.normalized()
+
+
+func _on_death_detection_body_entered(body: Node2D):
+	if body.is_in_group("Danger"):
+		Engine.time_scale = 0.3
+		$CollisionShape2D.queue_free()
+		$Timer.start()
+
 
 
 func _on_timer_timeout():
